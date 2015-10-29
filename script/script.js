0eb87f6690203e5fe7b2f438b9095f6d760f9124
Matthew Harris <hurris88@gmail.com>
2015-10-29 15:55:23 -0700

    Used jQuery's ajax method to call to TTH, instead of xhr request

--- script/script.js
+++ script/script.js
@@ -1,25 +1,30 @@
-var data = null;
+
 var totalpts = document.getElementById('totalpts');
 var htmlpts = document.getElementById('htmlpts');
 var csspts = document.getElementById('csspts');
 var jspts = document.getElementById('jspts');
 
-var xhr = new XMLHttpRequest();
+$.ajax({
+  url: "https://teamtreehouse.com/matharris.json"
+}).done(function(data) {;
+  totalpts.innerHTML = numberWithCommas(data.points.total);
+  htmlpts.innerHTML = numberWithCommas(data.points.HTML);
+  csspts.innerHTML = numberWithCommas(data.points.CSS);
+  jspts.innerHTML = numberWithCommas(data.points.JavaScript);
+});
 
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    var profile = JSON.parse(xhr.responseText);
-    totalpts.innerHTML = numberWithCommas(profile.points.total);
-    htmlpts.innerHTML = numberWithCommas(profile.points.HTML);
-    csspts.innerHTML = numberWithCommas(profile.points.CSS);
-    jspts.innerHTML = numberWithCommas(profile.points.JavaScript);
-  }
-}
-
-xhr.open("GET", "http://teamtreehouse.com/matharris.json");
-
-xhr.send(data);
-
+// var xhr = new XMLHttpRequest();
+//
+// xhr.onload = function() {
+//   if (xhr.status === 200) {
+//
+//   }
+// }
+//
+// xhr.open("GET", "https://teamtreehouse.com/matharris.json");
+//
+// xhr.send(data);
+//
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
