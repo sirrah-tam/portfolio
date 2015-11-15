ed67ebb092a6e4b75411f0a4811d4d54fb7e8379
Matthew Harris <hurris88@gmail.com>
2015-11-14 17:03:49 -0800

    Added badge functionality, pulling in last 3 badges earned

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ var totalpts = document.getElementById('totalpts');
 var htmlpts = document.getElementById('htmlpts');
 var csspts = document.getElementById('csspts');
 var jspts = document.getElementById('jspts');
+var $badgesDiv = $('#badges');
 
 $.ajax({
   url: "https://teamtreehouse.com/matharris.json"
@@ -11,6 +12,16 @@ $.ajax({
   htmlpts.innerHTML = numberWithCommas(data.points.HTML);
   csspts.innerHTML = numberWithCommas(data.points.CSS);
   jspts.innerHTML = numberWithCommas(data.points.JavaScript);
+  var badgeNum = data.badges.length - 1;
+  var lastthreebadge = data.badges.length - 3;
+  for ( var i = badgeNum; i >= lastthreebadge; i-- ){
+  	var badgeInfo = "";
+  	badgeInfo += "<img class='tthbadge' src=";
+  	badgeInfo += "'" + data.badges[i].icon_url + "'";
+  	badgeInfo += " />";
+  	badgeInfo += "<p>" + data.badges[i].name + "</p>";
+  	$badgesDiv.append(badgeInfo);
+  }
 });
 
 // var xhr = new XMLHttpRequest();
