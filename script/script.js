d38f179646085f247c2807c0b6906e5437b063a3
Matthew Harris <hurris88@gmail.com>
2015-11-15 15:32:42 -0800

    Updated styling and badge functionality, added points for WP, devtools, and PHP

--- script/script.js
+++ script/script.js
@@ -3,23 +3,30 @@ var totalpts = document.getElementById('totalpts');
 var htmlpts = document.getElementById('htmlpts');
 var csspts = document.getElementById('csspts');
 var jspts = document.getElementById('jspts');
+var wp = document.getElementById('wp');
+var devtools = document.getElementById('devtools');
+var php = document.getElementById('php');
 var $badgesDiv = $('#badges');
 
 $.ajax({
   url: "https://teamtreehouse.com/matharris.json"
-}).done(function(data) {;
+}).done(function(data) {
   totalpts.innerHTML = numberWithCommas(data.points.total);
   htmlpts.innerHTML = numberWithCommas(data.points.HTML);
   csspts.innerHTML = numberWithCommas(data.points.CSS);
   jspts.innerHTML = numberWithCommas(data.points.JavaScript);
+  wp.innerHTML = numberWithCommas(data.points.WordPress);
+  devtools.innerHTML = numberWithCommas(data.points['Development Tools']);
+  php.innerHTML = numberWithCommas(data.points.PHP);
   var badgeNum = data.badges.length - 1;
-  var lastthreebadge = data.badges.length - 3;
+  var lastthreebadge = data.badges.length - 4;
   for ( var i = badgeNum; i >= lastthreebadge; i-- ){
-  	var badgeInfo = "";
-  	badgeInfo += "<img class='tthbadge' src=";
+  	var badgeInfo = "<div class='tthbadge'>";
+  	badgeInfo += "<img src=";
   	badgeInfo += "'" + data.badges[i].icon_url + "'";
   	badgeInfo += " />";
   	badgeInfo += "<p>" + data.badges[i].name + "</p>";
+    badgeInfo += "</div>";
   	$badgesDiv.append(badgeInfo);
   }
 });
