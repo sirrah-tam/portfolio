78d1f08fa4aed4a65d55f6dbba0ecd8e11168bba
Mat Harris <hurris88@gmail.com>
2017-08-26 19:05:54 -0700

    ^ update index.php

--- index.php
+++ index.php
@@ -22,12 +22,8 @@
 			<div class="col-md-4 col-md-offset-1">
 				<img src="https://www.gravatar.com/avatar/6346dcbd2067cc872dc03286a69612fb?s=350" class="img-circle img-responsive center-block">
 			</div>
-			<div class="col-md-6 ">
+			<div class="col-md-6">
 				<h1 class="text-center">Mat Harris | Web Developer</h1>
-				<blockquote>
-					<p>When one man, for whatever reason, has the opportunity to lead an extraordinary life, he has no right to keep it to himself.</p>
-					<footer class="text-right">JACQUES COUSTEAU</footer>
-				</blockquote>
 			</div>
 		</div>
 	</section>
@@ -35,11 +31,102 @@
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
 				<h2 class="text-center">About Me</h2>
-				<p>I am a self-taught front-end web developer with an academic background in Political Science and International Relations. I got my start working in non-profit/association special projects and quickly realized that I excelled most when using code to make things more beautiful and efficient.</p>
+				<p>I am a front-end web developer with an academic background in Political Science and International Relations. I got my start working in non-profit/association special projects and quickly realized that I excelled most when using code to make things more beautiful and efficient.</p>
 
-				<p>I have learned by using online resources such as <a href="https://teamtreehouse.com/" target="_blank">Team Treehouse</a>, <a href="https://www.codecademy.com/" target="_blank">Codecademy</a>, <a href="https://www.codeschool.com/" target="_blank">CodeSchool</a>, and <a href="http://www.theodinproject.com/" target="_blank">The Odin Project</a> and also attended an in person JavaScript Course at <a href="https://generalassemb.ly/" target="_blank">General Assembly DC</a>.</p>
+				<p>2+ year experience in HTML, CSS, JavaScript, PHP, SQL, Git with working knowledge of WordPress development.</p>
 
-				<p>3+ year experience in HTML, CSS, JavaScript with working knowledge of PHP, WordPress development, version control using Git (GitHub and Bitbucket).</p>
+				<p>Currently working as a front-end developer at <a href="seattlewebdesign.com" target="_blank">efelle creative</a>, a digital agency located on the waterfront in downtown Seattle. I'm interested in community/cooperative development and focusing on making more accessible web experiences for all users.</p>
+			</div>
+		</div>
+	</section>
+	<section class="projects">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="header">
+					<div class="col-xs-2">
+						<span class="fa fa-code"></span>
+					</div>
+					<div class="col-xs-8">
+						<h2>Recent Projects</h2>
+					</div>
+					<div class="col-xs-2">
+						<span class="fa fa-caret-down"></span>
+					</div>
+				</div>
+				<div class="row profile">
+					<div class="project col-sm-4">
+						<figure>
+							<a href="https://epicvoyage.com/" target="_blank">
+								<img src="/img//epic-voyage.png" alt="Epic Voyage">
+							</a>
+						</figure>
+						<h3>Epic Voyage</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="https://culinex.biz/" target="_blank">
+								<img src="/dist/img/culinex.png" alt="Culinex">
+							</a>
+						</figure>
+						<h3>Culinex</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="https://nwsigns.com/" target="_blank">
+								<img src="/dist/img/nw-signs.png" alt="NW Signs">
+							</a>
+						</figure>
+						<h3>NW Signs</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="https://seapachomes.com/" target="_blank">
+								<img src="/dist/img/seapac-homes.png" alt="SEAPAC Homes">
+							</a>
+						</figure>
+						<h3>SEAPAC Homes</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="https://prestigecare.com/" target="_blank">
+								<img src="/dist/img/prestige-care.png" alt="Prestige Care">
+							</a>
+						</figure>
+						<h3>Prestige Care</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="http://www.mccaffreyassociates.com/" target="_blank">
+								<img src="/dist/img/mccaffrey-associates.png" alt="McCaffrey Associates">
+							</a>
+						</figure>
+						<h3>McCaffrey Associates</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="https://psetinsurance.com" target="_blank">
+								<img src="/dist/img/pset-insurance.png" alt="PSET Insurance">
+							</a>
+						</figure>
+						<h3>PSET Insurance</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="https://treeoflife-acupuncture.com" target="_blank">
+								<img src="/dist/img/treeoflife-acupuncture.png" alt="Tree of Life Accupuncture">
+							</a>
+						</figure>
+						<h3>Tree of Life Accupuncture</h3>
+					</div>
+					<div class="project col-sm-4">
+						<figure>
+							<a href="http://www.janofarchitecture.com/" target="_blank">
+								<img src="/dist/img/janof-architecture.png" alt="Janof Architecture">
+							</a>
+						</figure>
+						<h3>Janof Architecture</h3>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -49,14 +136,14 @@
 				<div class="header">
 					<div class="col-xs-2">
 						<a href="https://teamtreehouse.com/matharris">
-							<img src="dist/img/tth-logo.png">
+							<img class="icon" src="dist/img/tth-logo.png">
 						</a>
 					</div>
 					<div class="col-xs-8">
 						<h2>Team Treehouse Profile</h2>
 					</div>
 					<div class="col-xs-2">
-						<i class="fa fa-caret-down"></i>
+						<span class="fa fa-caret-down"></span>
 					</div>
 				</div>
 				<div class="row profile">
@@ -65,12 +152,12 @@
 							<p id="totalpts" class="totalpts"></p>
 							<p class="total">Total Points</p>
 							<ul>
-								<li><i class="fa fa-circle html"></i>HTML: <span id="htmlpts"></span></li>
-								<li><i class="fa fa-circle css"></i>CSS: <span id="csspts"></span></li>
-								<li><i class="fa fa-circle js"></i>JavaScript: <span id="jspts"></span></li>
-								<li><i class="fa fa-circle wp"></i>Wordpress: <span id="wp"></span></li>
-								<li><i class="fa fa-circle devtools"></i>Development Tools: <span id="devtools"></span></li>
-								<li><i class="fa fa-circle php"></i>PHP: <span id="php"></span></li>
+								<li><span class="fa fa-circle html"></span>HTML: <span id="htmlpts"></span></li>
+								<li><span class="fa fa-circle css"></span>CSS: <span id="csspts"></span></li>
+								<li><span class="fa fa-circle js"></span>JavaScript: <span id="jspts"></span></li>
+								<li><span class="fa fa-circle wp"></span>Wordpress: <span id="wp"></span></li>
+								<li><span class="fa fa-circle devtools"></span>Development Tools: <span id="devtools"></span></li>
+								<li><span class="fa fa-circle php"></span>PHP: <span id="php"></span></li>
 							</ul>
 							<h4><a href="https://teamtreehouse.com/matharris" target="_blank">See more at Team Treehouse >></a></h4>
 						</div>
@@ -90,14 +177,14 @@
 				<div class="header">
 					<div class="col-xs-2">
 						<a href="https://www.codeschool.com/users/sirrah_tam">
-							<img src="dist/img/codeschool-logo.png">
+							<img class="icon" src="dist/img/codeschool-logo.png">
 						</a>
 					</div>
 					<div class="col-xs-8">
 						<h2>CodeSchool Report Card</h2>
 					</div>
 					<div class="col-xs-2">
-						<i class="fa fa-caret-down"></i>
+						<span class="fa fa-caret-down"></span>
 					</div>
 				</div>
 				<div class="row profile">
@@ -137,10 +224,10 @@
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3">
 				<h2 class="text-center">Where To Find Me</h2>
-				<ul>
-					<li><i class="fa fa-envelope"></i>Email: <a href="mailto:sirrahtam@sirrahtam.com">sirrahtam@sirrahtam.com</a></li>
-					<li><i class="fa fa-twitter"></i>Twitter: <a href="https://twitter.com/sirrah_tam">@sirrah_tam</a></li>
-					<li><i class="fa fa-github"></i>Github: <a href="https://github.com/sirrah-tam">https://github.com/sirrah-tam</a></li>
+				<ul class="text-center">
+					<li><span class="fa fa-envelope"></span>Email: <a href="mailto:sirrahtam@sirrahtam.com">sirrahtam@sirrahtam.com</a></li>
+					<li><span class="fa fa-twitter"></span>Twitter: <a href="https://twitter.com/sirrah_tam">@sirrah_tam</a></li>
+					<li><span class="fa fa-github"></span>Github: <a href="https://github.com/sirrah-tam">https://github.com/sirrah-tam</a></li>
 				</ul>
 			</div>
 		</div>
